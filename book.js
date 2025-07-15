8d4de561a694e7dea6198aa3484ed5478a266730
harryL <harryleigh@hotmail.co.uk>
2025-07-15 09:30:06 +0100

    Updated Book to a ES6 class

--- book.js
+++ book.js
@@ -1,24 +1,25 @@
-export function Book(id, title, author, pages, read) {
-  this.id = id;
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
-  this.read = read;
+class Book {
+  constructor(id, title, author, pages, read) {
+    this.id = id;
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+  }
 
-  this.info = function () {
+  info() {
     return `ID: ${this.id}, ${this.title} by ${this.author}, ${
       this.pages
     } pages, ${hasRead()}`;
-  };
+  }
 
-  function hasRead() {
-    if (read) return "has read";
-    else return "not read yet";
+  hasRead() {
+    return this.read ? "has read" : "not read yet";
+  }
+
+  updateHasRead() {
+    this.read = !this.read;
   }
 }
-
-Book.prototype.updateHasRead = function () {
-  this.read = !this.read;
-};
 
 export default Book;
